9ec541499e6d1c64005b87b886e563cd37221ddc
HMangubat <mangubatherbert0902@gmail.com>
2025-06-12 21:56:56 +0800

    add login function

--- config/db.go
+++ config/db.go
@@ -9,7 +9,7 @@ import (
 
 // InitDB initializes and returns a DB connection.
 func InitDB() *sql.DB {
-	connStr := "postgres://postgres:123@10.9.2.30:5432/kafka?sslmode=disable"
+	connStr := "postgres://postgres:123@localhost:5432/hvm?sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalf("Fatal: could not connect to DB: %v", err)
--- models/user.go
+++ models/user.go
@@ -2,12 +2,17 @@ package models
 
 // User represents user data.
 type User struct {
-	ID            int     `json:"id"`
-	Username      string  `json:"username"`
-	Email         string  `json:"email"`
-	LatitudeDMS   string  `json:"latitudeDms"`
-	LongitudeDMS  string  `json:"longitudeDms"`
-	Latitude      float64 `json:"latitude"`
-	Longitude     float64 `json:"longitude"`
-	CreatedAt     string  `json:"createdAt"`
+	ID           int     `json:"id"`
+	Username     string  `json:"username"`
+	Email        string  `json:"email"`
+	Password     string  `json:"password,omitempty"`
+	Firstname    string  `json:"firstname"`
+	Middlename   string  `json:"middlename"`
+	Lastname     string  `json:"lastname"`
+	PasswordHash string  `json:"-"`
+	LatitudeDMS  string  `json:"latitudeDms"`
+	LongitudeDMS string  `json:"longitudeDms"`
+	Latitude     float64 `json:"latitude"`
+	Longitude    float64 `json:"longitude"`
+	CreatedAt    string  `json:"createdAt"`
 }
